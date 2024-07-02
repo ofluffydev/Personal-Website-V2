6b9297bccf4aa4c516db5b83b641c0741c672600
Kaden Frisk <fufferpish@gmail.com>
2024-07-02 18:47:21 -0500

    Updated my git ignore, added tailwind, and fixed some css issues. Also added a hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import Hero from "../components/home/Hero.tsx";
+
 function Home() {
     return (
         <div className={'homePage'}>
+            <Hero/>
             <p>Home page!</p>
         </div>
     )
