5e5d05e1046a629a1fd531b81613b89e3c1eaf8d
Kaden Frisk <fufferpish@gmail.com>
2024-07-03 20:03:05 -0500

    Update config and add stuff to html head!

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,7 @@ import {RobotOption} from 'sitemap-ts'
 const robotConfig: RobotOption[] = [{userAgent: '*', allow: '/'}]
 
 export default defineConfig({
-    plugins: [
-        react(),
-        Sitemap({
-            hostname: 'https://localhost:5173/',
-            robots: robotConfig,
-        })
-    ],
-    base: './',
+    plugins: [react(), Sitemap({
+        hostname: 'https://kadenfrisk.com/', robots: robotConfig,
+    })], base: './',
 })
